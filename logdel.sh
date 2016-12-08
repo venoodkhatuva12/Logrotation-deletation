1ac97fb0f8521ed64a10458f14c6227a9e6440e0
Venood Katuwa <vinod.katuwa12@gmail.com>
2016-12-08 17:33:03 +0530

    Update logdel.sh

--- logdel.sh
+++ logdel.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+#Script made for log deletion
+#Author: Vinod.N K
+#Usage: logs deletation of application or software
+#Distro : Linux -Centos, Rhel, and any fedora
+#Check whether root user is running the script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 #Catilina.log check
 
 SERVERNAME='/tomcat7/logs/'
